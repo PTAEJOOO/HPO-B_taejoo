7b4e004dc9b65db4faed52b68250e3d9800dd616
PTAEJOOO <qkrxown5985@gmail.com>
2024-04-04 14:37:06 +0900

    0404

---
+++ train_pfns4bo.py
@@ -0,0 +1,7 @@
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+from hpob_handler import HPOBHandler
+# from methods.pfns.pfns4bo import pfns4bo
+from methods.pfns.scripts.acquisition_functions import TransformerBOMethod
+
